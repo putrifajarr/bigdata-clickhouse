3cefd2051f286a1e60ac53c3e0641df71fcfab37
sehrigunawan <sehrigunawan@gmail.com>
2026-04-23 16:16:49 +0800

    feat: add cleaning logic for html and company suffix

--- scripts/02_clean_string.sql
+++ scripts/02_clean_string.sql
@@ -1,14 +1,8 @@
 SELECT 
-    -- Testing Point 2 & 5 (Company Name)
     company_name AS original_company,
-    trim(replaceRegexpAll(lower(company_name), '(/fl|/new|/de)$', '')) AS cleaned_company,
-
-    -- Testing Point 8 (HTML Stripping)
+    trim(replaceRegexpAll(lower(company_name), '(/fl|/new|/de|/inc|/corp)$', '')) AS cleaned_company,
     text AS original_text,
     replaceRegexpAll(replaceRegexpAll(text, '<[^>]+>', ''), '&[a-z#0-9]+;', '') AS cleaned_text,
-
-    -- Testing Point 3 (CIK Padding)
     cik_str AS original_cik,
     leftPad(cik_str, 10, '0') AS cleaned_cik
-FROM news_staging;
-LIMIT 100; -- Gunakan limit untuk testing di Step 2 Docker
+FROM praktikum.scrape_data_sample
